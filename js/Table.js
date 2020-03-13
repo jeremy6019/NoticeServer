9451e80ccf4d3004578b3e45850951b6515a36d4
kimkibeom <jeremy6019@daum.net>
2020-03-13 17:53:32 +0900

    ver1.0

---
+++ js/Table.js
@@ -0,0 +1,10 @@
+import { runInThisContext } from "vm";
+
+//테이블 정의 :ECMAScript 2015클래스, 상속지원 
+class Table{
+   
+    constructor(){//생성자 안에 멤버변수 정의 
+        this.table=document.createElement("table");
+        this.tr=
+    }
+}
